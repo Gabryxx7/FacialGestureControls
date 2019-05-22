1db4849fb0fc224e69a5cf2c3f9b20339161ce54
Gabriele <giaga7@gmail.com>
2019-05-22 11:27:21 +1000

    Update and windows version

--- AffectivaFaceGestures/app/src/main/java/com/unimelb/marinig/affectivafacegestures/MainActivity.java
+++ AffectivaFaceGestures/app/src/main/java/com/unimelb/marinig/affectivafacegestures/MainActivity.java
@@ -1,52 +1,348 @@
 package com.unimelb.marinig.affectivafacegestures;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.affectiva.android.affdex.sdk.Frame;
+import com.affectiva.android.affdex.sdk.detector.CameraDetector;
+import com.affectiva.android.affdex.sdk.detector.Detector;
+import com.affectiva.android.affdex.sdk.detector.Face;
+
+import java.util.List;
+
+/**
+ * This is a very bare sample app to demonstrate the usage of the CameraDetector object from Affectiva.
+ * It displays statistics on frames per second, percentage of time a face was detected, and the user's smile score.
+ *
+ * The app shows off the maneuverability of the SDK by allowing the user to start and stop the SDK and also hide the camera SurfaceView.
+ *
+ * For use with SDK 2.02
+ */
+public class MainActivity extends AppCompatActivity implements Detector.ImageListener, CameraDetector.CameraEventListener {
+
+    final String TAG = "CameraDetectorDemo";
+    private static final int RC_HANDLE_GMS = 9001;
+    // permission request codes need to be < 256
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
+
+    Button startSDKButton;
+    Button surfaceViewVisibilityButton;
+    TextView smileTextView;
+    TextView ageTextView;
+    TextView ethnicityTextView;
+    ToggleButton toggleButton;
+
+    SurfaceView cameraPreview;
+
+    boolean isCameraBack = false;
+    boolean isSDKStarted = false;
+
+    RelativeLayout mainLayout;
+
+    CameraDetector detector;
+
+    int previewWidth = 0;
+    int previewHeight = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        smileTextView = (TextView) findViewById(R.id.smile_textview);
+        ageTextView = (TextView) findViewById(R.id.age_textview);
+        ethnicityTextView = (TextView) findViewById(R.id.ethnicity_textview);
+
+        toggleButton = (ToggleButton) findViewById(R.id.front_back_toggle_button);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isCameraBack = isChecked;
+                switchCamera(isCameraBack? CameraDetector.CameraType.CAMERA_BACK : CameraDetector.CameraType.CAMERA_FRONT);
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        startSDKButton = (Button) findViewById(R.id.sdk_start_button);
+        startSDKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSDKStarted) {
+                    isSDKStarted = false;
+                    stopDetector();
+                    startSDKButton.setText("Start Camera");
+                } else {
+                    isSDKStarted = true;
+                    startDetector();
+                    startSDKButton.setText("Stop Camera");
+                }
+            }
+        });
+        startSDKButton.setText("Start Camera");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //We create a custom SurfaceView that resizes itself to match the aspect ratio of the incoming camera frames
+        mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
+        cameraPreview = new SurfaceView(this) {
+            @Override
+            public void onMeasure(int widthSpec, int heightSpec) {
+                int measureWidth = MeasureSpec.getSize(widthSpec);
+                int measureHeight = MeasureSpec.getSize(heightSpec);
+                int width;
+                int height;
+                if (previewHeight == 0 || previewWidth == 0) {
+                    width = measureWidth;
+                    height = measureHeight;
+                } else {
+                    float viewAspectRatio = (float)measureWidth/measureHeight;
+                    float cameraPreviewAspectRatio = (float) previewWidth/previewHeight;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    if (cameraPreviewAspectRatio > viewAspectRatio) {
+                        width = measureWidth;
+                        height =(int) (measureWidth / cameraPreviewAspectRatio);
+                    } else {
+                        width = (int) (measureHeight * cameraPreviewAspectRatio);
+                        height = measureHeight;
+                    }
+                }
+                setMeasuredDimension(width,height);
+            }
+        };
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_TOP,RelativeLayout.TRUE);
+        cameraPreview.setLayoutParams(params);
+        mainLayout.addView(cameraPreview,0);
+
+        surfaceViewVisibilityButton = (Button) findViewById(R.id.surfaceview_visibility_button);
+        surfaceViewVisibilityButton.setText("HIDE SURFACE VIEW");
+        surfaceViewVisibilityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cameraPreview.getVisibility() == View.VISIBLE) {
+                    cameraPreview.setVisibility(View.INVISIBLE);
+                    surfaceViewVisibilityButton.setText("SHOW SURFACE VIEW");
+                } else {
+                    cameraPreview.setVisibility(View.VISIBLE);
+                    surfaceViewVisibilityButton.setText("HIDE SURFACE VIEW");
+                }
+            }
+        });
+
+        int rc = ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            initializeDetector();
+        } else {
+            requestCameraPermission();
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Handles the requesting of the camera permission.  This includes
+     * showing a "Snackbar" message of why the permission is needed then
+     * sending the request.
+     */
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions, RC_HANDLE_CAMERA_PERM);
+            }
+        };
+    }
+
+    /**
+     * Callback for the result from requesting permissions. This method
+     * is invoked for every call on {@link #requestPermissions(String[], int)}.
+     * <p>
+     * <strong>Note:</strong> It is possible that the permissions request interaction
+     * with the user is interrupted. In this case you will receive empty permissions
+     * and results arrays which should be treated as a cancellation.
+     * </p>
+     *
+     * @param requestCode  The request code passed in {@link #requestPermissions(String[], int)}.
+     * @param permissions  The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *                     which is either {@link PackageManager#PERMISSION_GRANTED}
+     *                     or {@link PackageManager#PERMISSION_DENIED}. Never null.
+     * @see #requestPermissions(String[], int)
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission granted - initialize the camera source");
+            // we have permission, so create the camerasource
+            initializeDetector();
+            return;
+        }
+
+        Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        /*AlertDialog.Builder builder = new AlertDialog(this);
+        builder.setTitle("Face Tracker sample")
+                .setMessage("NO CAMERA PERMISSION")
+                .setPositiveButton("OK", listener)
+                .show();*/
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isSDKStarted) {
+            //startDetector();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //stopDetector();
+    }
+
+    void initializeDetector(){
+        detector = new CameraDetector(this, CameraDetector.CameraType.CAMERA_FRONT, cameraPreview);
+        detector.setDetectSmile(true);
+        detector.setDetectAllEmotions(true);
+        detector.setDetectAllEmojis(true);
+        detector.setDetectAllExpressions(true);
+        detector.setDetectAllAppearances(true);
+        //detector.setDetectAge(true);
+        //detector.setDetectEthnicity(true);
+        detector.setImageListener(this);
+        detector.setOnCameraEventListener(this);
+    }
+
+    void startDetector() {
+        if (!detector.isRunning()) {
+            detector.start();
+        }
+    }
+
+    void stopDetector() {
+        if (detector.isRunning()) {
+            detector.stop();
+        }
+    }
+
+    void switchCamera(CameraDetector.CameraType type) {
+        detector.setCameraType(type);
+    }
+
+    @Override
+    public void onImageResults(List<Face> list, Frame frame, float v) {
+        if (list == null)
+            return;
+        if (list.size() == 0) {
+            smileTextView.setText("NO FACE");
+            ageTextView.setText("");
+            ethnicityTextView.setText("");
+        } else {
+            Face face = list.get(0);
+            Log.e(TAG, String.format("WINK Emoji\n%.2f",face.emojis.getWink()));
+            Log.e(TAG, String.format("Eye closure\n%.2f",face.expressions.getEyeClosure()));
+            Log.e(TAG, String.format("Eye closure\n%.2f",face.expressions));
+            Log.e(TAG, String.format("Left Eye Landmark\n%.2f"));
+            smileTextView.setText(String.format("SMILE\n%.2f",face.expressions.getSmile()));
+            switch (face.appearance.getAge()) {
+                case AGE_UNKNOWN:
+                    ageTextView.setText("");
+                    break;
+                case AGE_UNDER_18:
+                    ageTextView.setText(R.string.age_under_18);
+                    break;
+                case AGE_18_24:
+                    ageTextView.setText(R.string.age_18_24);
+                    break;
+                case AGE_25_34:
+                    ageTextView.setText(R.string.age_25_34);
+                    break;
+                case AGE_35_44:
+                    ageTextView.setText(R.string.age_35_44);
+                    break;
+                case AGE_45_54:
+                    ageTextView.setText(R.string.age_45_54);
+                    break;
+                case AGE_55_64:
+                    ageTextView.setText(R.string.age_55_64);
+                    break;
+                case AGE_65_PLUS:
+                    ageTextView.setText(R.string.age_over_64);
+                    break;
+            }
+
+            switch (face.appearance.getEthnicity()) {
+                case UNKNOWN:
+                    ethnicityTextView.setText("");
+                    break;
+                case CAUCASIAN:
+                    ethnicityTextView.setText(R.string.ethnicity_caucasian);
+                    break;
+                case BLACK_AFRICAN:
+                    ethnicityTextView.setText(R.string.ethnicity_black_african);
+                    break;
+                case EAST_ASIAN:
+                    ethnicityTextView.setText(R.string.ethnicity_east_asian);
+                    break;
+                case SOUTH_ASIAN:
+                    ethnicityTextView.setText(R.string.ethnicity_south_asian);
+                    break;
+                case HISPANIC:
+                    ethnicityTextView.setText(R.string.ethnicity_hispanic);
+                    break;
+            }
+
+        }
+    }
+
+    @SuppressWarnings("SuspiciousNameCombination")
+    @Override
+    public void onCameraSizeSelected(int width, int height, Frame.ROTATE rotate) {
+        if (rotate == Frame.ROTATE.BY_90_CCW || rotate == Frame.ROTATE.BY_90_CW) {
+            previewWidth = height;
+            previewHeight = width;
+        } else {
+            previewHeight = height;
+            previewWidth = width;
+        }
+        cameraPreview.requestLayout();
     }
 }
