8ed602d985fbc881276e15bc24901e0951b70ad6
ebibabaei <49933290+ebibabaei@users.noreply.github.com>
2019-05-22 11:43:19 +1000

    Added Project

---
+++ WindowsAffectiva/HelloAffectiva/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Affdex;
+using System.Reflection;
+
+namespace HelloAffectiva
+{
+    public partial class Form1 : Form, Affdex.ImageListener
+    {
+        public Form1(Affdex.Detector detector)
+        {
+            detector.setImageListener(this);
+
+            InitializeComponent();
+        }
+
+        public void onImageCapture(Frame frame)
+        {
+            frame.Dispose();
+        }
+
+        public void onImageResults(Dictionary<int, Face> faces, Frame frame)
+        {
+            foreach (KeyValuePair<int, Affdex.Face> pair in faces)
+            {
+                Affdex.Face face = pair.Value;
+                if(face!=null)
+                {
+                    foreach(PropertyInfo prop in typeof(Affdex.Emotions).GetProperties())
+                    {
+                        float Value = (float)prop.GetValue(face.Emotions, null);
+                        string output = string.Format("{0}: {1:0.00}", prop.Name, Value);
+                        System.Console.WriteLine(output);
+                    }
+                }
+            }
+            frame.Dispose();       
+        }
+    }
+}
---
+++ WindowsAffectiva/HelloAffectiva/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloAffectiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Affdex.Detector detector = new Affdex.CameraDetector(0,30,30,3,Affdex.FaceDetectorMode.LARGE_FACES);
+            Form1 feed = new Form1(detector); 
+            detector.setClassifierPath("C:\\Program Files\\Affectiva\\AffdexSDK\\data");
+            detector.setDetectAllEmotions(true);
+            detector.setDetectBrowRaise(true);
+            detector.setDetectSmile(true);
+            detector.setDetectGender(true);
+            detector.setDetectGlasses(true);
+            detector.start();
+            feed.ShowDialog();
+            detector.stop();
+        }
+    }
+}
+ 
